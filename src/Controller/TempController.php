6daf8791430bd4ab350059ac6ed4767be33eb7da
Ram <koscheibismark@yandex.ru>
2018-12-01 16:03:15 +0300

    First commit

---
+++ src/Controller/TempController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TempController extends AbstractController
+{
+    /**
+     * @Route("/temp", name="temp")
+     */
+    public function index()
+    {
+        return $this->render('temp/index.html.twig', [
+            'controller_name' => 'TempController',
+        ]);
+    }
+}
